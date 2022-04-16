942941217cc3e8b49ebb52fb84041408326c364e
Mohammad Nur Rizki <mnurrizki25@gmail.com>
2022-04-16 13:38:05 +0700

    fix: Add port

--- demo.js
+++ demo.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT|| 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
